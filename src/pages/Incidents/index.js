3e6dcdd43f2d189cc9cecfdb58111d285effde4e
Pedro Henrique Fabriz Ulhoa <fabriz_pedro@MacBook-Air-de-Pedro.local>
2020-04-04 18:00:08 -0300

    mobile

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoTree from '../../assets/just_tree 1.png';
+import logoUbuntu from '../../assets/logoubuntu 1.png';
+
+import styles from './styles';
+
+export default function Incidents(){
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                
+                <Text>Olá</Text>
+                <Text>Olá</Text>
+                <Text>Olá</Text>
+                <Text>Olá</Text>
+            </View>
+        </View>
+    );
+}
